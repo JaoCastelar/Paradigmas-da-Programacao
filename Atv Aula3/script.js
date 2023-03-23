345beedb5a19d10032e3a4b425cf13a2854055a0
JaoCastelar <joao.castelar@estudante.iftm.edu.br>
2023-03-23 15:39:11 -0300

    aula 3

---
+++ Atv Aula3/script.js
@@ -0,0 +1,18 @@
+
+let cep = document.getElementById("cep")
+
+function validaCep( cep ) {
+
+
+    
+}
+
+function printa(txt, clear=false){
+
+    let box = document.getElementById("box");
+
+    if ( clear )
+        box.innerHTML = "";
+
+    box.innerHTML += txt + "<br />";
+}
