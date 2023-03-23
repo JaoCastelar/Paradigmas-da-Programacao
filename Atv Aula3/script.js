2ad236937cb5e0e79d03cff3ad71878b24367d29
JaoCastelar <joao.castelar@estudante.iftm.edu.br>
2023-03-23 16:46:53 -0300

    atv aula3

--- Atv Aula3/script.js
+++ Atv Aula3/script.js
@@ -1,18 +1,23 @@
 
+let regexCEP = /^[0-9]{5}-?[0-9]{3}$/;
 let cep = document.getElementById("cep")
 
-function validaCep( cep ) {
-
-
-    
-}
-
 function printa(txt, clear=false){
-
-    let box = document.getElementById("box");
+    let box = document.getElementById("resp");
 
     if ( clear )
         box.innerHTML = "";
 
     box.innerHTML += txt + "<br />";
+}
+
+function validaCep(cep) {
+
+    if (regexCEP.test(cep.value)) {
+        printa("CEP válido", true)
+    }else{
+        printa("CEP inválido", true)
+    }
+    
+    
 }
